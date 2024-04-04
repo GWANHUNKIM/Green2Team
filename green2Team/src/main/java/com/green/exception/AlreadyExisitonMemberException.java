b3b70f7c62e6aeba4f07e43aa7021af229e5d988
Doritos <mydjkim@naver.com>
2024-04-04 10:03:28 +0900

    update Doritos

--- green2Team/src/main/java/com/green/exception/AlreadyExisitonMemberException.java
+++
@@ -1,9 +0,0 @@
-package com.green.exception;
-
-public class AlreadyExisitonMemberException extends RuntimeException {
-
-	public AlreadyExisitonMemberException(String msg) {
-		super(msg);
-	}
-	
-}
--- test-1/src/main/java/com/green/exception/IdPasswordNotMatchingException.java
+++
@@ -1,9 +0,0 @@
-package com.green.exception;
-
-public class IdPasswordNotMatchingException extends RuntimeException {
-
-	public IdPasswordNotMatchingException(String msg) {
-		super(msg);
-	}
-
-}
